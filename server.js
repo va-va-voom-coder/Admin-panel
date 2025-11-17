972dcf8887cd62064479aad5a0fdedc11975fbed
va-va-voom-coder <parmarmrunalini27@gmail.com>
2025-11-17 16:50:14 +0530

    server.js error

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-const port = process.env.PORT || ;
+const port = process.env.PORT || 10000 ;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
