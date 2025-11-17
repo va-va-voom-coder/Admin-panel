cb612d36053ae21f25db88f6b606c54546e80a8b
va-va-voom-coder <parmarmrunalini27@gmail.com>
2025-11-17 17:12:12 +0530

    deploy error

--- server.js
+++ server.js
@@ -3,18 +3,22 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-const port = process.env.PORT || 10000;
 
+// Needed for ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Serve dist folder (Vite build output)
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.use((req, res) => {
+// For React Router
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
+// Render will pass PORT
+const PORT = process.env.PORT || 10000;
 
-app.listen(port, "0.0.0.0", () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
