c8ef381e8536191c2db7741c56b404ca2875f747
va-va-voom-coder <parmarmrunalini27@gmail.com>
2025-11-17 16:52:32 +0530

    error solve

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-const port = process.env.PORT || 10000 ;
+const port = process.env.PORT || 10000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
